1f091f9616bbfe1d1ebd835f2b76d5e4c2a46cef
Johnn81100 <103576060+Johnn81100@users.noreply.github.com>
2022-08-26 01:19:29 +0200

    upgrate programmation

--- entreprise1/script.js
+++ entreprise1/script.js
@@ -22,4 +22,55 @@ all.forEach(a => {
         }
     } )
 })      
+// const scroll= document.getElementById("scroll");
+// const accueil = document.getElementById("accueil");
+// let height = accueil.clientHeight;
 
+// const scroll1= document.getElementById("scroll1");
+// const prestation= document.getElementById("prestation");
+// let height1 = prestation.clientHeight; 
+
+// const scroll2= document.getElementById("scroll2");
+// const catalogue= document.getElementById("catalogue");
+// let height2 = catalogue.clientHeight; 
+
+// window.addEventListener("scroll",() =>
+// {
+//     console.log(window.scrollY)
+//     if(  window.scrollY < height )
+//     {
+//         scroll.classList.add("active");
+//     }
+//     else
+//     {
+//         scroll.classList.remove("active");
+//     }
+// })
+
+
+// window.addEventListener("scroll",() =>
+// {
+//     console.log(window.scrollY)
+//     if(  window.scrollY<height1 )
+//     {
+//         scroll1.classList.add("active");
+//     }
+//     else
+//     {
+//         scroll1.classList.remove("active");
+//     }
+// })
+
+
+// window.addEventListener("scroll",() =>
+// {
+//     console.log(window.scrollY)
+//     if( window.scrollY>height1 & window.scrollY<)
+//     {
+//         scroll1.classList.add("active");
+//     }
+//     else
+//     {
+//         scroll1.classList.remove("active");
+//     }
+// })
