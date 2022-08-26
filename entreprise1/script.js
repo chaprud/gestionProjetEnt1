0cf6ba4afbdd4ae9e08e766853351bdbdfe42108
Johnn81100 <103576060+Johnn81100@users.noreply.github.com>
2022-08-26 14:31:42 +0200

    upgrade

--- entreprise1/script.js
+++ entreprise1/script.js
@@ -11,15 +11,23 @@ console.log(all);
 all.forEach(a => {
     a.addEventListener("click",(e)=>
     {
-        console.log(e.target);
-        e.target.classList.add("active"); /* ajoute  la classe active sur la cible  que la fonction ecoute l evenement  clique*/
+        // console.log(e.target);
+        // e.target.classList.add("active"); /* ajoute  la classe active sur la cible  que la fonction ecoute l evenement  clique*/
+        // for(let i=0;i<all.length; i++)
+        // {
+        //     if(all[i] !== e.target) /* si  le li est strictement different du li cible   au clic alors on enléve  la classe active. */
+        //         {
+        //             all[i].classList.remove("active"); /*alors on enléve  la classe active.*/
+        //         }      
+        // }
+          // e.target.classList.add("active"); /* ajoute  la classe active sur la cible  que la fonction ecoute l evenement  clique*/
         for(let i=0;i<all.length; i++)
         {
-            if(all[i] !== e.target) /* si  le li est strictement different du li cible   au clic alors on enléve  la classe active. */
-                {
-                    all[i].classList.remove("active"); /*alors on enléve  la classe active.*/
-                }      
+            all[i].classList.remove("active"); /*alors on enléve  la classe active.*/
+
+                
         }
+        e.target.classList.add("active");
     } )
 })      
 // const scroll= document.getElementById("scroll");
